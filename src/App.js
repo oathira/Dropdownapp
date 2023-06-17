53fb7cf8b1cbed01afa5e043bc995f2e416a4178
athira <athiraos1997@gmail.com>
2023-06-18 05:06:05 +0530

    add deopdown app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+
 
 function App() {
+  const [elements] = useState(['Yes','Probably Not']);
+  const [value,setValue] = useState('');
+  const [ishover,setHover] = useState(false);
+
+  const handleMouseEnter = () =>{
+    setHover(true);
+  }
+  
+  const handleOnclick = (e) => {
+    setValue(e.target.textContent);
+    setHover(false);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "dropdown">
+      <h1>Should you use a dropdown?</h1>
+      <h3>{value}</h3>
+      
+      <button className = "select-btn" onMouseEnter = {handleMouseEnter} >Select</button>
+      <div className = "options" style = { { display: `${ ishover ? 'block' : 'none' }` } }>
+        
+        {  elements.map((elem,index) => {
+              return(
+                       <a href = "#"  onClick = {handleOnclick} key = {index} >{elem}</a>
+                    );
+              })
+        }
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+ 
+  
+ 
