fd9fd7ee10fd2f1437cbff8b52829318437d1370
athira <athiraos1997@gmail.com>
2023-06-18 05:55:17 +0530

    added comments

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import { useState } from "react";
 
-
-
 function App() {
+  //useState hooks used in functional component to update the state
   const [elements] = useState(['Yes','Probably Not']);
   const [value,setValue] = useState('');
   const [ishover,setHover] = useState(false);
-
+ //when mouse is hover over the select button this function will trigger
   const handleMouseEnter = () =>{
     setHover(true);
   }
-  
+  //when clicking on an option display updated value
   const handleOnclick = (e) => {
     setValue(e.target.textContent);
     setHover(false);
   }
-  
+  //inside return we use jsx format
   return (
     <div className = "dropdown">
       <h1>Should you use a dropdown?</h1>
@@ -23,10 +22,12 @@ function App() {
       
       <button className = "select-btn" onMouseEnter = {handleMouseEnter} >Select</button>
       <div className = "options" style = { { display: `${ ishover ? 'block' : 'none' }` } }>
-        
+       {/* map function to iterate the elements list  */}
         {  elements.map((elem,index) => {
               return(
-                       <a href = "#"  onClick = {handleOnclick} key = {index} >{elem}</a>
+                       <a href = "#"  onClick = {handleOnclick} key = {index} >
+                         {elem}
+                        </a>
                     );
               })
         }
